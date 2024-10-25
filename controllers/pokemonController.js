b9d81abf46a4ce4f5581a55802a67aeb36d6ac16
danilbalistero <danilbalistero@gmail.com>
2024-10-25 14:42:24 -0300

    criando formulario e css

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,4 +1,4 @@
-const pokemonModel = require ('../models/pokemonModel');
+const pokemonModel = require('../models/pokemonModel');
 
 const getAllPokemons = (req, res) => {
     const pokemons = pokemonModel.getPokemons();
@@ -6,12 +6,22 @@ const getAllPokemons = (req, res) => {
 };
 
 const getPokemon = (req, res) => {
-    const pokemon = pokemonModel.getPokemonById(req.paramns.id);
+    const pokemon = pokemonModel.getPokemonById(req.params.id);
     if (pokemon) {
         res.render('pokemon', { pokemon });
     } else {
-        res.status(404).send('Pokémon não encontrado');
+        res.status(404).send('Pokemon não encontrado');
     }
 };
 
-module.exports = {getAllPokemons, getPokemon};
+const getCreatePokemonForm = (req, res) => {
+    res.render('criarPokemon');
+};
+
+const createPokemon = (req, res) => {
+    const { nome, tipo } = req.body;
+    pokemonModel.createPokemon(nome, tipo);  // Chamando o método no modelo
+    res.redirect('/');  // Redireciona para a página inicial após o cadastro
+};
+
+module.exports = { getAllPokemons, getPokemon, getCreatePokemonForm, createPokemon };
--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pokemonController = require('../controllers/pokemonController');
 
+
 router.get('/', pokemonController.getAllPokemons);
 router.get('/pokemon/:id', pokemonController.getPokemon);
+router.get('/pokemons/new', pokemonController.getCreatePokemonForm);
+router.post('/pokemons', pokemonController.createPokemon);
 
 module.exports = router;
